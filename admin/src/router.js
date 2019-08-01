4568769a74a57357f549b05a6b45b711e257818c
abps <shit_map@hotmail.com>
2019-08-01 11:02:30 +0800

    快速生成'物品'管理 子路由 测试

--- admin/src/router.js
+++ admin/src/router.js
@@ -3,8 +3,12 @@ import Router from 'vue-router'
 // import Home from './views/Home.vue'
 
 import Main from './views/Main.vue'
+
 import CategoryEdit from './views/CategoryEdit.vue'
 import CategoryList from './views/CategoryList.vue'
+
+import ItemEdit from './views/ItemEdit.vue'
+import ItemList from './views/ItemList.vue'
 
 Vue.use(Router)
 
@@ -16,7 +20,11 @@ export default new Router({
         children: [
             { path: '/categories/create', component: CategoryEdit },
             { path: '/categories/edit/:id', component: CategoryEdit, props: true },
-            { path: '/categories/list', component: CategoryList }
+            { path: '/categories/list', component: CategoryList },
+
+            { path: '/items/create', component: ItemEdit },
+            { path: '/items/edit/:id', component: ItemEdit, props: true },
+            { path: '/items/list', component: ItemList },
         ]
     }]
 })
